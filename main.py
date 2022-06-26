9a45244130781f73ae1db26b4f79f5a83142df2f
PilarczykM <marcinpilarczyk@me.com>
2022-06-26 15:01:06 +0200

    Update main.py
    Main program

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from solid import Order
+
+
 def main():
-    print("hello")
+    order = Order()
+    order.add_item("Keyboard", 1, 50)
+    order.add_item("SSD", 2, 150)
+    order.add_item("USB cable", 3, 5)
+
+    print(order.total_price())
+    order.pay("debit", "123456")
 
 
 if __name__ == '__main__':
