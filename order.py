06c82d3394778c96f73b0d35be91c6a27d019f07
PilarczykM <marcinpilarczyk@me.com>
2022-06-26 15:18:03 +0200

    Correct responsibility for Order class
    Rename from solid to order

---
+++ order.py
@@ -0,0 +1,19 @@
+class Order:
+    items = []
+    quantities = []
+    prices = []
+    __status = "open"
+
+    def add_item(self, name: str, quantity: int, price: float) -> None:
+        self.items.append(name)
+        self.quantities.append(quantity)
+        self.prices.append(price)
+
+    def total_price(self):
+        total = 0
+        for i in range(len(self.prices)):
+            total += self.quantities[i] * self.prices[i]
+        return total
+
+    def set_paid_status(self):
+        self.__status = "paid"
